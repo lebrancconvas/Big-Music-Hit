0f8e06ce48b1d78966c95f7ba584088bffd20470
lebrancconvas <lebrancconvas@gmail.com>
2022-05-11 19:01:36 +0700

    ✨ Create API Server Response.

--- server/server.py
+++ server/server.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/api/v1/musics')
+@app.route('/')
+def index():
+  return "Welcome to Big Music Hit Server."
+
+@app.route('/api/v1/musics', methods=['GET'])
 def musics():
-  return {"members": ["John", "Mary", "Bob"]}
+  req = requests.get('http://localhost:9000/musics')
+  data = req.content
+  json_data = json.loads(data)
+  return render_template('index.html', data=json_data)
 
 if __name__ == "__main__":
   app.run(debug=True)
