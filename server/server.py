4b6e4e1dd701c85a1b61a5b816799d4b2f82e77b
lebrancconvas <lebrancconvas@gmail.com>
2022-05-11 19:39:18 +0700

    🔨 Add CORS in Flask API Server.

--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
 from flask import Flask, render_template
+from flask_cors import CORS, cross_origin
 import requests
 import json
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
   return "Welcome to Big Music Hit Server."
 
 @app.route('/api/v1/musics', methods=['GET'])
+@cross_origin()
 def musics():
   req = requests.get('http://localhost:9000/musics')
   data = req.content
