85457716f4aa5a4661de25864f89f2d892ac3739
lebrancconvas <lebrancconvas@gmail.com>
2022-05-12 21:29:45 +0700

    ✨ Get API from JSON Server + Add CORS.

--- fast-server/server.py
+++ fast-server/server.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
 import uvicorn
+import requests
+import json
 
 app = FastAPI()
 
+origins = [
+	"http://localhost:3000"
+]
+
+app.add_middleware(
+	CORSMiddleware,
+	allow_origins=origins,
+	allow_credentials=True,
+	allow_methods=["*"],
+	allow_headers=["*"]
+)
+
 @app.get('/')
-def index():
-  return "Welcome to FASTAPI Server." 
+async def index():
+  url = "http://localhost:9000/musics"
+  req = requests.get(url)
+  data = req.content
+  json_data = json.loads(data)
+  return json_data
