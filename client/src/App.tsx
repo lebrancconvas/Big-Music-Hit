5bfbb39a748d6cfd993dbb0127df9943f7cb33b6
lebrancconvas <lebrancconvas@gmail.com>
2022-05-12 11:33:47 +0700

    💄 Update Web Background Color.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import MusicList from './pages/MusicList';
+import './App.css'
 
 const App: React.FC = () => {
   return(
