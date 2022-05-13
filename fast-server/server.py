157303d84914d2b240f9cd8f7d2853e561283ba7
lebrancconvas <lebrancconvas@gmail.com>
2022-05-13 23:00:35 +0700

    🔨 Create Album API Data.

--- fast-server/server.py
+++ fast-server/server.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+from secret import *
+from album_list import *
 import pandas as pd
 import uvicorn
 import requests
@@ -16,7 +18,8 @@ origins = [
 SPOTIPY_REDIRECT_URI = "http://localhost:8000/data"
 SCOPE = "user-top-read"
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=SPOTIFY_CLIEND_ID, client_secret=SPOTIFY_CLIENT_SECRET, redirect_uri=SPOTIPY_REDIRECT_URI, scope=SCOPE))
+auth_manager = SpotifyClientCredentials(client_id=SPOTIFY_CLIEND_ID, client_secret=SPOTIFY_CLIENT_SECRET)
+sp = spotipy.Spotify(auth_manager=auth_manager)
 
 app.add_middleware(
 	CORSMiddleware,
@@ -26,14 +29,32 @@ app.add_middleware(
 	allow_headers=["*"]
 )
 
+def ms_to_minsec(ms):
+  min = int((ms / 1000) / 60)
+  sec = int((ms / 1000) % 60)
+  unit_int = [0,1,2,3,4,5,6,7,8,9]
+  if(sec in unit_int):
+    return(f'{min}:0{sec}')
+  return(f'{min}:{sec}')
+
 @app.get('/')
 async def index():
-	top_track = sp.current_user_top_tracks(limit=10, offset=0, time_range="short_term")
-	# json_data = json.load(top_track)
-	return top_track
-# async def index():
-#   url = "http://localhost:9000/musics"
-#   req = requests.get(url)
-#   data = req.content
-#   json_data = json.loads(data)
-#   return json_data
+  track_list_response = []
+  albums = sp.album(album_data['album_2']) 
+  artist_name = albums['artists'][0]['name']
+  album_name = albums['name']
+  track_list = albums['tracks']['items']
+  album_image = albums['images'][1]['url']
+  for i in range(0, len(track_list)):
+    track_data_response = {}
+    track_data_response['id'] = i + 1
+    track_data_response['track'] = track_list[i]['name']
+    track_data_response['artist'] = artist_name
+    track_data_response['album'] = album_name
+    track_data_response['duration'] = ms_to_minsec(track_list[i]['duration_ms'])
+    track_data_response['url'] = track_list[i]['external_urls']['spotify']
+    track_data_response['image'] = album_image
+    track_list_response.append(track_data_response)
+  return track_list_response 
+
+  # return albums 
