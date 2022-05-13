5f4492b153b916e39d13c333a83ed9aa26bb9a25
lebrancconvas <lebrancconvas@gmail.com>
2022-05-13 23:30:12 +0700

    🔨 Create Album Link.

--- fast-server/album_list.py
+++ fast-server/album_list.py
@@ -1,5 +1,7 @@
 album_list = {
   'album_1': 'https://open.spotify.com/album/73mih5aIcuQ4OaQLI7O0X9?si=uSkIX6D-S8WE1Ub9le94Zw',
   'album_2': 'https://open.spotify.com/album/6bm2zfTwTBJ66rObupBJxS?si=kNuZsRqMQIuZPKiSirmQkw',
-  'album_3': 'https://open.spotify.com/album/2GsgGploNr5Mq65PgOiU2u?si=Vx88nheOTRu7603ahLpTSg'
+  'album_3': 'https://open.spotify.com/album/2GsgGploNr5Mq65PgOiU2u?si=Vx88nheOTRu7603ahLpTSg',
+  'album_4': '',
+  'album_5': 'https://open.spotify.com/album/55SKL390IVlFrLs9KDDIZQ?si=LL2RiO8tSKCcICoVgpdN8A'
 }
